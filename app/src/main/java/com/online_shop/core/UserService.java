4937800a81724125ad7b7c327dff83e35dd0dd32
Nastya Hlopovskaya <xlnastya@gmail.com>
2021-03-27 12:00:44 +0300

    init

---
+++ app/src/main/java/com/online_shop/core/UserService.java
@@ -0,0 +1,29 @@
+package com.online_shop.core;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface UserService {
+    @GET("login.php")
+    Call<Integer> login(@Query("login") String login, @Query("password") String password);
+
+    @GET("register.php")
+    Call<Integer> register(
+            @Query("name_") String name_,
+            @Query("surname") String surname,
+            @Query("email") String email,
+            @Query("password_") String password_,
+            @Query("phonenumber") String phonenumber,
+            @Query("address") String address
+    );
+
+    @GET("update.php")
+    Call<Integer> update(
+            @Query("id_") Integer user_id,
+            @Query("field") String field,
+            @Query("value_") String value);
+
+   // @GET("makeOrder.php")
+  //  Call <Integer> makeOrder(@Query("order") Order order);
+}
